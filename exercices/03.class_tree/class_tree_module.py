5ed99b2f8cb8f564e8b332f358f692043206aa9a
boson-gamma-kplr <130389833+boson-gamma-kplr@users.noreply.github.com>
2023-05-10 09:57:12 +0000

    finished class tree exercice

--- exercices/03.class_tree/class_tree_module.py
+++ exercices/03.class_tree/class_tree_module.py
@@ -3,7 +3,7 @@ from unidecode import unidecode
 from treelib import Tree
 import os
 
-def json_dict_from_file():
+def json_dict_from_file(file_name):
     """
     Cette fonction ouvre et charge les données JSON du fichier
     dans un dictionnaire Python.
@@ -14,7 +14,7 @@ def json_dict_from_file():
     # Get the directory path of the current Python file
     local_path = os.path.dirname(os.path.abspath(__file__))
     # Chargement des données JSON à partir du fichier dans un dictionnaire python
-    json_data = json.load(open(os.path.join(local_path, 'json_data.json'), "rb"))
+    json_data = json.load(open(os.path.join(local_path, file_name), "rb"))
     
     # il est nécessaire de reconvertir le dictionnaire en chaine de caractere pour le traiter ensuite
     json_str = json.dumps(json_data)
@@ -28,8 +28,27 @@ def json_dict_from_file():
 
     return json_dict
 
-def create_tree_from_dict(json_dict):
+def create_tree_from_dict(tree, parent_node_id, parent_dict):
     """
-    Fonction pour créer un arbre à partir d'un dictionnaire Python
+    Cette fonction crée un arbre à partir d'un dictionnaire.
+    Elle est appelée récursivement pour chaque sous-dictionnaire.
+
+    Args:
+        tree (Tree): un objet Tree de la bibliothèque treelib pour représenter l'arbre
+        parent_node_id (str): l'identifiant du noeud parent dans l'arbre
+        parent_dict (dict): le dictionnaire Python contenant les données à insérer dans l'arbre
     """
-    
+    for key, value in parent_dict.items():
+        if isinstance(value, dict):
+            # Créer un nouveau noeud pour la clé courante du dictionnaire
+            new_node_id = parent_node_id
+            if(key != "subclasses"):
+                new_node_id = f"{parent_node_id}.{key}"
+                tree.create_node(tag=key, identifier=new_node_id, parent=parent_node_id)
+            
+            # Créer récursivement le sous-arbre pour le dictionnaire courant
+            create_tree_from_dict(tree, new_node_id, value)
+        # else:
+        #     # Créer un nouveau noeud pour la feuille courante du dictionnaire
+        #     leaf_node_id = f"{parent_node_id}.{key}"
+        #     tree.create_node(tag=f"{key}: {value}", identifier=leaf_node_id, parent=parent_node_id)
--- exercices/03.class_tree/main_class_tree.py
+++ exercices/03.class_tree/main_class_tree.py
@@ -1,7 +1,28 @@
-import json
-from unidecode import unidecode
 from treelib import Tree
 
 from class_tree_module import json_dict_from_file
+from class_tree_module import create_tree_from_dict
 
 
+def main():
+    """
+    Cette fonction est la fonction principale qui orchestre toutes les autres.
+    Elle charge les données JSON depuis un fichier, crée un objet Tree de la bibliothèque treelib,
+    et crée un arbre à partir des données JSON.
+
+    Elle affiche ensuite l'arbre créé.
+    """
+    my_tree = Tree()
+    # Créer le noeud racine pour l'arbre
+    my_tree.create_node(tag="Product Classes Hierarchy", identifier="racine")
+
+    # Charger les données JSON depuis un fichier et créer la structure de l'arbre à partir du dictionnaire
+    json_dict = json_dict_from_file('json_data.json')
+    create_tree_from_dict(my_tree, "racine", json_dict)
+
+    # Afficher l'arbre
+    my_tree.show()
+
+if __name__ == '__main__':
+    # Appeler la fonction principale
+    main()
