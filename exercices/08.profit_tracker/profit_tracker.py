591cfc72d165c1450a784c147ff19448cf2474b1
boson-gamma-kplr <130389833+boson-gamma-kplr@users.noreply.github.com>
2023-05-11 10:11:18 +0000

    starting profit_tracker

--- exercices/08.profit_tracker/profit_tracker.py
+++ exercices/08.profit_tracker/profit_tracker.py
@@ -1,16 +1,20 @@
 # La classe ProfitTracker est utilisée pour suivre les profits du magasin.
+import sys
+sys.path.extend(['.','..','/workspaces/Python-OOP-Project/exercices/05.inventory_product_entry','/workspaces/Python-OOP-Project/exercices/06.inventory_manager'])
+from product_classes import Product
 
 class ProfitTracker:
 
     # Le constructeur initialise la variable balance (solde)
     def __init__(self):
         # Créer une variable 'balance' et l'initialiser à 1000 euros
-
+        self._balance = 1000
     #Méthode buy_product 
-    """   
-    La méthode buy_product est utilisée pour acheter un produit et mettre à jour le coût total et le solde.
-    """     
+        
     def buy_product(self, product: Product, quantity): 
+        """   
+        La méthode buy_product est utilisée pour acheter un produit et mettre à jour le coût total et le solde.
+        """
         """
         Vérifie si le solde est suffisant pour acheter la quantité demandée de produit
             Si le solde est insuffisant:
